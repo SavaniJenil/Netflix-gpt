e81177a6b49ff3a6326998cd94ff0a6f07dbba1f
jenil Savani <Savanijenil.Nitinbhai@Solera.com>
2023-09-22 18:49:11 +0530

    Install firebase, Add userStore, Authenticate app, Host App

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,53 @@
-
+import { useNavigate } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    return(
-        <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-[2]">
-            <img className="w-40" src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png" alt="logo" />
+  const navigate = useNavigate();
+
+  const userInfo = useSelector((store) => store.user);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
+
+  return (
+    <div className="absolute bg-gradient-to-b from-black z-[2]  w-full flex justify-between items-center">
+      <img
+        className="w-60"
+        src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png"
+        alt="app-logo"
+      />
+      {userInfo && (
+        <div className="flex flex-row">
+          <img
+            className="w-10 h-10 rounded-md"
+            src={
+              userInfo.photoURL
+                ? userInfo.photoURL
+                : "https://occ-0-1946-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbCL6LUIHvRieXJLEY0Jq6AxJmhEiLpZ-9U42s2-XxOQLeA5D71cQBnph9xdZBQpwi-5SET231ylQqok29OIepgmRzHUEzY.png?r=7ae"
+            }
+            alt="user-logo"
+          />
+          <button
+            className="mx-2 text-white font-semibold"
+            onClick={handleSignOut}
+          >
+            (Sign Out)
+          </button>
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 export default Header;
